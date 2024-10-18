f0e57ed3c259072539139f5add9c880153c6475c
Cool <tyryshkinartem100@gmail.com>
2024-10-18 13:24:27 +0300

    мега большое изменение бд

--- Uchebka123k4s1/Data/Remote/SqlModel/Supplier.cs
+++ Uchebka123k4s1/Data/Remote/SqlModel/Supplier.cs
@@ -24,7 +24,7 @@ namespace Uchebka123k4s1.Data.Remote.SqlModel
         public int Id { get; set; }
         public string Title { get; set; }
         public string Address { get; set; }
-        public Nullable<System.DateTime> DeliveryTime { get; set; }
+        public Nullable<System.TimeSpan> DeliveryTime { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Hardware> Hardware { get; set; }
